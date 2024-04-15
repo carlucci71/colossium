944ca8443335965b05455bb0d1f9686133e3de3a
Daniele <carlucci.daniele@gmail.com>
2024-04-15 09:30:08 +0000

    upd

--- lancia.sh
+++ lancia.sh
@@ -1,3 +1,2 @@
-cd /home/daniele/githubrepository/colossium
-mvn spring-boot:run
-
+cd /home/daniele/github/colossium
+mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Xmx256m -Xms128m" -Dspring-boot.run.arguments=tipoElaborazione=$1>> log.log 2>>err.log &
